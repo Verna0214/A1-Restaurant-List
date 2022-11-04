de007dc399b758173fdecb54e9e1eeb7cdb756b8
Verna0214 <yuhsichen0214@gmail.com>
2022-11-04 11:27:31 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -23,11 +24,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 const router = express.Router()
 const User = require('../../models/user')
 
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
   res.render('login')
 })
 
@@ -19,30 +19,46 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   //取得表單資料，並運用解構複值
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '星號欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   // 檢查是否已註冊過
   // 因為名字非必填，所以用email做條件檢視
   User.findOne({ email }).then(user => {
     // 如果user資料是true，代表註冊過
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => {
-          console.log(err)
-          res.render('error', { errorMsg: err.message })
-        })
     }
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => {
+        console.log(err)
+        res.render('error', { errorMsg: err.message })
+      })
   })
   .catch(err => {
     console.log(err)
@@ -52,6 +68,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
